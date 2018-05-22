75e44c51528ec1398436baca67bd85958317f2a8
furiaelebegraph <webmaster@elebegraph.com>
2018-05-22 17:12:52 -0500

    'maqueta'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,87 +8,117 @@
         <title>Puerta Romani</title>
 
         <!-- Fonts -->
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/helpers.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/general.css') }}">
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+        <div class="row justify-content-center align-items-center">
+            <div class="col-12">
+                <div class="portada">
+                    <img src="{{ asset('img/portada_1.jpg') }}" alt="">
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            </div>
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-2">
+                        <a href="#quienes">QUIENES SOMOS?</a>
+                    </div>
+                    <div class="col-2">
+                        <a href="#modelos">MODELOS</a>
+                    </div>
+                    <div class="col-2">
+                        <a href="#amenidades">AMENIDADES</a>
+                    </div>
+                    <div class="col-2">
+                        <a href="#ubicacion">UBICACION</a>
+                    </div>
+                    <div class="col-2">
+                        <a href="#contacto">CONTACTO</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="pestana">
+                    <img src="" alt="">
+                    <p>QUIENES SOMOS?</p>
+                </div>
+                <div class="centro_880">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="{{ asset('img/galeria_1.jpg') }}" alt="">
+                        </div>
+                        <div class="col-6">
+                            <div class="row">
+                                <div class="col-12">
+                                    <img src="{{ asset('img/galeria_2.jpg') }}" alt="">
+                                </div>
+                                <div class="col-12">
+                                    <img src="{{ asset('img/galeria_3.jpg') }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <img src="{{ asset('img/galeria_4.jpg') }}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="centro_880">
+                    <div class="row">
+                        <div class="col-12">
+                            <img src="" alt="">
+                        </div>
+                        <div class="col-12">
+                            <h2>-Porta Romani Residencial-</h2>
+                        </div>
+                        <div class="col-12">
+                            <p>
+                                Es un desarrollo inmobiliario donde la calidad es la prioridad sobre todas las cosas y que fue diseñado cuidando cada detalle.
+
+                                Con una ubicación privilegiada al sur de la ciudad de León, Guanajuato, se desarrolla sobre una superficie de más de tres hectáreas y media un complejo habitacional de 137 viviendas con tres modelos diferentes y una zona comercial de 22 locales.
+
+                                Con un acceso moderno, áreas verdes inmejorables y espacios funcionales hacen de Porta Romani Residencial un proyecto habitacional de alta plusvalía. 
+
+                            </p>
+                        </div>
+                        <div class="col-12">
+                            <img src="" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <a href="">
+                    <img src="genova.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-12">
+                <a href="">
+                    <img src="monza.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-12">
+                <a href="">
+                    <img src="verona.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-12">
+                <div class="centro_880">
+                    <div class="row">
+                        <div class="col-4 col-sm-2"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="contacto">
+                    
+                </div>
+                <div id="map">
+                    
                 </div>
             </div>
         </div>
