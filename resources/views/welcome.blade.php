aa80fdb39a34c1859ce8f014d5a9a358c1010f53
Emmanuel Vazquez <emmanegr@iMac-de-Emmanuel.local>
2018-05-22 23:57:55 -0500

    modificaicones welcomes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,6 +92,21 @@
                 </div>
             </div>
             <div class="col-12">
+                <a href="">
+                    <img src=" {{ asset('img/genova.jpg') }}  " alt="">
+                </a>
+            </div>
+            <div class="col-12">
+                <a href="">
+                    <img src=" {{ asset('img/monza.jpg') }} " alt="">
+                </a>
+            </div>
+            <div class="col-12">
+                <a href="">
+                    <img src=" {{ asset('img/verona.jpg') }}" alt="">
+                </a>
+            </div> 
+            <div class="col-12">
                 
                 <div class="pestana">
                     <img src="" alt="">
@@ -99,25 +114,46 @@
                 </div>
             </div>
             <div class="col-12">
-                <a href="">
-                    <img src="genova.jpg" alt="">
-                </a>
-            </div>
-            <div class="col-12">
-                <a href="">
-                    <img src="monza.jpg" alt="">
-                </a>
-            </div>
-            <div class="col-12">
-                <a href="">
-                    <img src="verona.jpg" alt="">
-                </a>
-            </div>
-            <div class="col-12">
                 <div class="centro_880">
                     <div class="row">
                         <div class="col-4 col-sm-2">
-                            
+                            <img src="{{ asset('img/amenidades1.png') }}" alt="">
+                            <p>SEGURIDAD
+
+                                Contamos con accesos resguardados por un excelente equipo de seguridad, brindando de esta manera, el mejor cuidado para tu hogar y tus seres queridos.
+                            </p>
+                        </div>
+                        <div class="col-4 col-sm-2">
+                            <img src="{{ asset('img/amenidades2.png') }}" alt="">
+                            <p>VIALIDADES
+
+                                Contamos con un sistema de vialidades pensadas para la comodidad de los tuyos, en donde tu y tu familia puedan transitar libremente sin nigun peligro.
+                            </p>
+                        </div>
+                        <div class="col-4 col-sm-2">
+                            <img src="{{ asset('img/amenidades3.png') }}" alt="">
+                            <p>ÁREAS VERDES
+
+                                Somos un residencial sumamente responsable con el cuidado del medio ambiente, por lo que contamos con grandes secciones de áreas verdes para recreación de todos nuestros habitantes.
+¡Vive momentos únicos!
+                            </p>
+                        </div>
+                        <div class="col-4 col-sm-2">
+                            <img src="{{ asset('img/amenidades4.png') }}" alt="">
+                            <p>ÁREAS RECREATIVAS
+
+
+                                Pensado para los pequeños de la familia, contamos con espacios para la recreación y diversión familiar. 
+¡Atesora momentos inolvidables!
+                            </p>
+                        </div>
+                        <div class="col-4 col-sm-2">
+                            <img src="{{ asset('img/amenidades5.png') }}" alt="">
+                            <p>ÁPRIVACIDAD TOTAL
+
+
+                                Disfruta de una mayor tranquilidad y momentos de reflexión y calma dentro de nuestro residencial, relajate y déjate llevar con la mayor privacidad.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -135,15 +171,15 @@
                             Av. Porta Romani 100 Col...
                             Telefóno <a href="tel:+1 477 000 0000"> +1 (477) 000 0000</a>
                         </div>
-                    </div>
 
-                    <div class="col-12 col-md-6">
-                        <form action="enviarCorreo">
-                            <input type="text" placeholder="Nombre Completo" class="form-control">
-                            <input type="text" placeholder="E-mail" class="form-control">
-                            <input type="text" placeholder="Mensaje" class="form-control">
-                            <input type="submit" class='btn_submit_porta' value='ENVIAR'>
-                        </form>
+                        <div class="col-12 col-md-6">
+                            <form action="enviarCorreo">
+                                <input type="text" placeholder="Nombre Completo" class="form-control">
+                                <input type="text" placeholder="E-mail" class="form-control">
+                                <input type="text" placeholder="Mensaje" class="form-control">
+                                <input type="submit" class='btn_submit_porta' value='ENVIAR'>
+                            </form>
+                        </div>
                     </div>
                     
                 </div>
